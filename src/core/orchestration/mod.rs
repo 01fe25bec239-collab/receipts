d06b1a6a03c3f744b2cbe7102e91e67f276acaf2
Omkar Gavale <01fe25bec239@kletech.ac.in>
2026-09-24 20:51:26 +0530

    feat(orchestration): add FeatureCapabilitySet physical contract

--- src/core/orchestration/mod.rs
+++ src/core/orchestration/mod.rs
@@ -2,6 +2,7 @@
 
 mod date_time;
 mod feature_admission_decision;
+mod feature_capability_set;
 mod json_value;
 mod remote_policy;
 
@@ -9,6 +10,9 @@ pub use date_time::{OrchestrationDateTimeError, OrchestrationDateTimeV1};
 pub use feature_admission_decision::{
     FeatureAdmissionDecision, FeatureAdmissionDecisionError, FeatureAdmissionOutcome,
     FeatureAdmissionUpgradeInfo,
+};
+pub use feature_capability_set::{
+    FeatureCapability, FeatureCapabilitySet, FeatureCapabilitySetError, FeatureCapabilityStatus,
 };
 pub use json_value::{
     OrchestrationJsonNumberError, OrchestrationJsonNumberV1, OrchestrationJsonObjectV1,
@@ -21,6 +25,9 @@ mod date_time_tests;
 
 #[cfg(test)]
 mod feature_admission_decision_tests;
+
+#[cfg(test)]
+mod feature_capability_set_tests;
 
 #[cfg(test)]
 mod json_value_tests;
